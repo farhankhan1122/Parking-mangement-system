be0f2a8d4df6f7bad1e7dfae036a8fb57d9b7cfe
Farhan Khan <kfarhan634@MacBook-Air.local>
2025-07-04 10:02:48 +0530

    login api checked working

--- login/login.component.ts
+++ login/login.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { LoginService } from '../../services/login-service/login.service';
+import { LoginRequestData, LoginResponseData } from '../../models/login.model';
+import { error } from 'console';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginObj: LoginRequestData = new LoginRequestData();
+  loginService = inject(LoginService)
+
+ngOnInit(): void {
+    console.log(this.loginObj);
+  }
+
+  onLogin() {
+    this.loginService.loginUser(this.loginObj).subscribe((res: LoginResponseData) => {
+      alert('user found ... navigating')
+    },error => {
+      alert('wrong credentials')
+    })
+  }
 
 }
